c18bd97a9b46be61d0c09319bd474a0aec040ebf
Lawrence <lawrencevaldez027@gmail.com>
2025-10-01 18:17:00 +0800

    chore : udpate name

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function BabyBossCelebration() {
     "Rose Jay Mabini",
     "Arian Joy Ramirez",
     "Navin Santiago",
-    "Laurie Ann Larada",
+    "Laurie Ann Rarada",
     "Sednie Kaye Tauyan",
     "Crislen Estuesta",
     "Lowella Ranada",
